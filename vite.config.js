3e1c5e38f7fddeab6d20f6a8b3588ad998694dea
ayoub-benismain <ayoubbenismain@gmail.com>
2025-01-16 19:54:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // vite.config.js
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['react-icons/md'] // Add the external module here
+    }
+  }
+});
 export default {
   build: {
     rollupOptions: {
